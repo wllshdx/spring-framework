0263e38537f6ae1ac3c1d93f8541e21e0cdecb69
weiliangliang <weiliangliang@imdada.cn>
2022-01-14 18:07:24 +0800

    [feat]succeed to build

---
+++ wll-mvc-study/src/main/java/cn/wllnb/domain/MyBean.java
@@ -0,0 +1,14 @@
+package cn.wllnb.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author weiliangliang
+ */
+@Data
+@AllArgsConstructor
+public class MyBean {
+	private int age;
+	private String name;
+}
